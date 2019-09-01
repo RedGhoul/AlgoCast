1b9d13487d7c7bc5cb4508845aaeb54060fa23fa
RedGhoul <avaneesab5@gmail.com>
2019-09-01 06:47:55 -0400

    did maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  return MaxCharWayOne(str);
+}
+
+function MaxCharWayOne(str) {
+  let map = {};
+  str = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (map[str[i]]) {
+      map[str[i]]++;
+    } else {
+      map[str[i]] = 1;
+    }
+  }
+  let maxValue = 0;
+  let charMax = '';
+  for (x in map) {
+    if (map[x] > maxValue) {
+      maxValue = map[x];
+      charMax = x;
+    }
+  }
+  return charMax;
+}
 
 module.exports = maxChar;
