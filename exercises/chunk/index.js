591261c36fcf8b672ddaf231308d6cb65aed9c11
RedGhoul <avaneesab5@gmail.com>
2019-09-03 19:37:34 -0400

    python fix buzz - chunk js 3

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,54 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  return chunkWay3(array, size);
+}
+
+function chunkWay3(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+function chunkWay2(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    let piece = chunked[chunked.length - 1];
+
+    if (!piece || piece.length === size) {
+      // so we are adding a new chuck in there
+      chunked.push([element]);
+    } else {
+      piece.push(element);
+    }
+  }
+  return chunked;
+}
+function chunkWay1(array, size) {
+  let answer = [];
+  let temparr = [];
+  if (size === 0) {
+    return array;
+  }
+  let temp = size;
+  for (let x = 0; x < array.length; x++) {
+    if (x % temp != 0 || x === 0) {
+      temparr.push(array[x]);
+    } else {
+      answer.push(temparr);
+      temparr = [];
+      temparr.push(array[x]);
+    }
+  }
+  answer.push(temparr);
+  return answer;
+}
 
 module.exports = chunk;
