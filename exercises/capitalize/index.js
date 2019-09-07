18334e631258401fd1b6020ca63ce2dc3f88b3a2
RedGhoul <avaneesab5@gmail.com>
2019-09-06 22:05:43 -0400

    added capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,41 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return way1(str);
+}
+
+function way1(str) {
+  // asumming there will always be spaces seprating words
+
+  str = str.split(' ');
+
+  for (let i = 0; i < str.length; i++) {
+    str[i] =
+      str[i].split('')[0].toUpperCase() +
+      str[i]
+        .split('')
+        .slice(1)
+        .join('');
+  }
+
+  //slice(start(which is included, end which is not inculded))
+
+  return str.join(' ');
+}
+
+function way2(str) {
+  // asumming there will always be spaces seprating words
+
+  const words = [];
+
+  for (let word of str.split('')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  //slice(start(which is included, end which is not inculded))
+
+  return words.join(' ');
+}
 
 module.exports = capitalize;
