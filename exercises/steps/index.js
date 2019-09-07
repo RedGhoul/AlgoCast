b0e14ebf377be93fe1c2ef51a8cca962292d54db
RedGhoul <avaneesab5@gmail.com>
2019-09-07 08:51:32 -0400

    added steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,50 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  stepss(n);
+}
 
+function way1(n) {
+  for (let i = 1; i < n + 1; i++) {
+    let output = '';
+    for (let y = 0; y < i; y++) {
+      output = output + '#';
+    }
+    for (let x = i; x < n; x++) {
+      output = output + ' ';
+    }
+    console.log(output);
+  }
+}
+
+function way2(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+function stepss(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return stepss(n, row + 1);
+  }
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  stepss(n, row, stair);
+}
 module.exports = steps;
