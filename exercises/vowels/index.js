ee8fce286a6129369fe22601bf39858119b45cd4
RedGhoul <avaneesab5@gmail.com>
2019-09-08 09:47:10 -0400

    did vowelCount

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,43 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  return way3(str);
+}
 
+function way1(str) {
+  str = str.toLowerCase().split('');
+  let vowelCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === 'a' ||
+      str[i] === 'e' ||
+      str[i] === 'i' ||
+      str[i] === 'o' ||
+      str[i] === 'u'
+    ) {
+      vowelCount++;
+    }
+  }
+
+  return vowelCount;
+}
+
+function way2(str) {
+  let count = 0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function way3(str) {
+  // using regex
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 module.exports = vowels;
