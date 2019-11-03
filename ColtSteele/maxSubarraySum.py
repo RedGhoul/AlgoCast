8725661e427b0bbe0c206350b34f039a97019ebc
RedGhoul <avaneesab5@gmail.com>
2019-11-03 08:15:10 -0500

    did another way of maxSubarraySum

--- ColtSteele/maxSubarraySum.py
+++ ColtSteele/maxSubarraySum.py
@@ -22,10 +22,55 @@ def maxSubarraySum(arr, n):
     
     return highestValue
 
+
+"""
+Another way of doing this would have been to keep a running sum. And a move of the window
+would be done through subtracting something from the sum and adding something to the sum
+"""
+
+def maxSumAlt(arr, n):
+    if len(arr) == 0 or n == 0:
+        return None
+
+    maxSum = 0
+    tempSum = 0
+    # range does not include the last thing 
+    # if you give it range 0,4 it will go from 0 -> 2
+    # creating the starting sum here
+    for x in range(0,n): # this is just giving you the index
+        maxSum += arr[x]
+
+    tempSum = maxSum
+    
+
+    for x in range(n,len(arr)): #range doesn't include the max (thing on the right)
+        #        the thing just behind   the thing just infront
+        tempSum = tempSum - arr[x - n] + arr[x]
+        # if tempSum > maxSum:
+        #     maxSum = tempSum
+        # you can also do
+        maxSum = max(tempSum,maxSum)
+
+    return maxSum
+
+
+    
+
+
+
 if __name__ == "__main__":
+    
+    print("maxSubarraySum")
     print(maxSubarraySum([1,2,5,2,8,1,5],2))
     print(maxSubarraySum([1,2,5,2,8,1,5], 4))
     print(maxSubarraySum([4,2,1,6],1))
     print(maxSubarraySum([4,2,1,6,2],4))
     print(maxSubarraySum([],4))
+
+    print("maxSumAlt")
+    print(maxSumAlt([1,2,5,2,8,1,5], 2))
+    print(maxSumAlt([1,2,5,2,8,1,5], 4))
+    print(maxSumAlt([4,2,1,6],1))
+    print(maxSumAlt([4,2,1,6,2],4))
+    print(maxSumAlt([],4))
 
