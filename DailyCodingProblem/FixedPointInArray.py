715b5626605a2c0b4a83c159ae13e4574c0b4202
RedGhoul <avaneesab5@gmail.com>
2019-11-06 13:09:47 -0500

    fixedpointBinary

--- DailyCodingProblem/FixedPointInArray.py
+++ DailyCodingProblem/FixedPointInArray.py
@@ -20,5 +20,36 @@ def fixedpoint(arr):
 
     return False
 
+"""
+Time: O(log(n))
+Space: O(n)
+"""
+def helper(arr):
+    return (0,len(arr))
+
+def fixedpointBinary(arr, minv = None, maxv = None):
+    if minv == None and maxv == None:
+        minv = helper(arr)[0]
+        maxv = helper(arr)[1]
+
+    if len(arr) == 0:
+        return False
+
+    mid = round((maxv-minv) / 2)
+
+    if mid == arr[mid]:
+        return mid
+
+    if mid > minv:
+        return fixedpointBinary(arr[mid:(maxv+1)],mid,maxv)
+
+    elif mid < minv:
+        return fixedpointBinary(arr[minv:(mid+1)],minv,mid)
+
+    return False
+
+
 if __name__ == "__main__":
     print(fixedpoint([-6,0,2,40]))
+    print(fixedpointBinary([-6,0,2,40]))
+    print(fixedpointBinary([-6,0,33,33,3,33,40]))
