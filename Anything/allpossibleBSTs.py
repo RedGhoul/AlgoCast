4447c244e4b25b6e53b17ae4428976fa246bbac3
RedGhoul <avaneesab5@gmail.com>
2019-11-11 22:43:25 -0500

    added UTF8EncodingCheck among other things

---
+++ Anything/allpossibleBSTs.py
@@ -0,0 +1,4 @@
+"""
+EASY - Rating
+Given an integer N, construct all possible binary search trees with N nodes.
+"""
---
+++ Anything/efficientstringmatching.py
@@ -0,0 +1,14 @@
+"""
+HARD - Rating
+Implement an efficient string matching algorithm.
+
+That is, given a string of length N and a pattern of length k, write a program that searches for the pattern in the string with less than O(N * k) worst-case time complexity.
+
+If the pattern is found, return the start index of its location. If not, return False.
+
+Thoughts:
+Seems like you should just implement the KMP string matching algo to get a complexity of O(K+N)
+"""
+
+def efficientstringmatching(searchString, pattern):
+    pass
