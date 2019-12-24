7c413fa67905e2a034de7b33adebf77bc43dcaf8
RedGhoul <avaneesab5@gmail.com>
2019-12-24 10:01:07 -0500

    added remove edge and vertex

--- ColtSteele/DS/Graph.py
+++ ColtSteele/DS/Graph.py
@@ -28,13 +28,45 @@ class Graph:
         else:
             self.AdjacencyList[vertex2] = [vertex1]
 
+    def removeEdge(self,vertex1,vertex2):
+
+        if vertex1 in self.AdjacencyList:
+            self.AdjacencyList[vertex1].remove(vertex2)
+        if vertex2 in self.AdjacencyList:
+            self.AdjacencyList[vertex2].remove(vertex1)
+
+    def removeVertex(self,vertex):
+
+        if vertex in self.AdjacencyList:
+            del self.AdjacencyList[vertex]
+            for key,val in self.AdjacencyList.items():
+                if vertex in val:
+                    val.remove(vertex)
+    
+    def removeVertex2(self,vertex):
+        #using removeEdge
+        if vertex in self.AdjacencyList:
+
+            while len(self.AdjacencyList[vertex]) > 0:
+                adjVertex = self.AdjacencyList[vertex].pop()
+                self.AdjacencyList[vertex].append(adjVertex)
+                self.removeEdge(adjVertex,vertex)
+            
+            del self.AdjacencyList[vertex]
+        
+
+
+    
+
 
 if __name__ == "__main__":
     graphy = Graph()
 
     graphy.addVertex(3)
     graphy.addVertex(23)
-
+    graphy.addVertex(44)
     graphy.addEdge(2,3)
     graphy.addEdge(23,3)
+    graphy.addEdge(23,44)
+    graphy.removeVertex2(2)
     print(graphy.AdjacencyList)
