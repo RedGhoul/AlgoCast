1c64047afc6c2ad7aeb74490bd697ae3ed83b907
RedGhoul <avaneesab5@gmail.com>
2020-01-23 23:12:51 -0500

    re did fib

--- StephenGrider/exercises/Java/JavaJ/src/main/java/com/stephengrider/problems/Fibonacci.java
+++ StephenGrider/exercises/Java/JavaJ/src/main/java/com/stephengrider/problems/Fibonacci.java
@@ -5,42 +5,91 @@ import java.util.HashMap;
 
 public class Fibonacci {
 
+	// counting from Zero
 	public static void main(String[] args) {
-		System.out.println(recursive_Fibonacci_Memo(29, null));
+		int input = 200;
+//		long startTime = System.currentTimeMillis();
+//
+//		System.out.println(fibonacci_Find_Recur(input));
+//
+//		long endTime = System.currentTimeMillis();
+//
+//		System.out.println("That took " + (endTime - startTime) + " milliseconds + fibonacci_Find_Recur");
+
+
+		long startTime2 = System.currentTimeMillis();
+
+		System.out.println(fibonacci_Find_Iter(input));
+
+		long endTime2 = System.currentTimeMillis();
+
+		System.out.println("That took " + (endTime2 - startTime2) + " milliseconds + fibonacci_Find_Iter");
+
+
+		long startTime3 = System.currentTimeMillis();
+
+		System.out.println(fibonacci_Find_Recur_Memo( input,null));
+
+		long endTime3 = System.currentTimeMillis();
+
+		System.out.println("That took " + (endTime3 - startTime3) + " milliseconds + fibonacci_Find_Recur_Memo");
+
+
 	}
-	
-	public static Integer recursive_Fibonacci(Integer number) {
-		if(number == 0) {
+
+	public static Integer fibonacci_Find_Recur_Memo(int sequenceNumber, HashMap<Integer,Integer> map){
+		if(map == null){
+			map = new HashMap<Integer, Integer>();
+		}
+		if(sequenceNumber < 0){
+			return null;
+		}
+		if(sequenceNumber == 0){
 			return 0;
-		}else if(number == 1) {
+		}else if(sequenceNumber == 1){
 			return 1;
 		}
-		
-		return recursive_Fibonacci(number - 1) + 
-			   recursive_Fibonacci(number - 2);
-	}
-	
-	public static Integer recursive_Fibonacci_Memo(Integer number, HashMap<Integer,Integer> memory) {
-		if(memory == null) {
-			memory = new HashMap<Integer,Integer>();
+
+		if(map.containsKey(sequenceNumber)){
+			return map.get(sequenceNumber);
 		}
-		
-		if(memory.containsKey(number)) {
-			return memory.get(number);
-		}
-		else if(number == 0) {
-			return 0;
-		}else if(number == 1) {
-			return 1;
-		}
-		
-		Integer answer = recursive_Fibonacci_Memo(number - 1, memory) + 
-				recursive_Fibonacci_Memo(number - 2,memory);
-		
-		memory.put(number, answer);
-		
-		System.out.println(answer);
-		
+
+		Integer answer = fibonacci_Find_Recur_Memo(sequenceNumber-1,map) + fibonacci_Find_Recur_Memo(sequenceNumber-2,map);
+
+		map.put(sequenceNumber,answer);
+
 		return answer;
 	}
+	public static Integer fibonacci_Find_Recur(int number){
+		if(number < 0){
+			return null;
+		}
+		if(number == 0) {
+			return 0;
+		} else if(number == 1){
+			return 1;
+		}
+
+		return fibonacci_Find_Recur(number-1) + fibonacci_Find_Recur(number-2);
+	}
+
+	public static Integer fibonacci_Find_Iter(int number){
+		if(number == 0){
+			return 0;
+		}else if(number == 1){
+			return 1;
+		}
+
+		Integer first = 0;
+		Integer second = 1;
+		Integer finalAnswer = 0;
+		for(int i =1; i < number; i++){
+			finalAnswer = first + second;
+			first = second;
+			second = finalAnswer;
+		}
+
+		return finalAnswer;
+	}
+
 }
