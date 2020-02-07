b02b7a5cfda668b7da8ca0f8065d6ab5fe4f27b8
RedGhoul <avaneesab5@gmail.com>
2020-02-07 09:16:30 -0500

    did selection sort properly

--- StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/BubbleSort.java
+++ StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
      * You could also do this one while loop plus a for loop
      *
      * **/
-    public static int[] bubbleSortV1(int[] array) {
+    public static int[] bubbleSortV2(int[] array) {
         for(int i =0; i < array.length; i++){
             for(int j = i + 1;j < array.length; j++){
                 if(array[i] > array[j]){
--- StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/SelectionSort.java
+++ StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/SelectionSort.java
@@ -2,22 +2,38 @@ package com.AlgoExpert.problems;
 
 public class SelectionSort {
     /**
-     * Same thing as bubble sort expect you are checking if anything
-     * is less then. Rather then greater than. And you make the swap.
+     * Your building things from the back of the array to the front if the array
      *
-     * By doing it this way you are building the sorted array from the back
-     * (least to greatest) rather then from (greatest to least) aka bubble sort
+     * Your checking to see if your smallestIndex is actually your smallest index
      *
+     * Every time you find something smaller, you make the smallestIndex point to
+     * the new smallest index
+     *
+     * Then you keep comparing till you get to the end of the array
+     *
+     * Then finally you swap the current smallest index with what ever index i is at.
+     *
+     * Your esstentially going through the array and trying to find the smallest number.
+     * And then swapping it with current index which is going left to right
+     * so you have a sorted array being built up behind the current value of i
      * **/
     public static int[] selectionSort(int[] array) {
-        for(int i = 0; i < array.length; i++){
+        for(int i = 0; i < array.length-1; i++){
+
+            int smallestIndex = i;
+
             for(int j = i+1; j < array.length; j++){
-                if(array[i] > array[j]){
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                if(array[smallestIndex] > array[j]){
+                    smallestIndex = j;
                 }
             }
+
+            if(smallestIndex != i){
+                int temp = array[i];
+                array[i] = array[smallestIndex];
+                array[smallestIndex] = temp;
+            }
+
         }
         return array;
     }
