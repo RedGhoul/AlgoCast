476961d11701a8f37546f92d636082c73c120a4e
RedGhoul <avaneesab5@gmail.com>
2020-02-11 22:08:35 -0500

    learned some things

--- StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/numberOfWaysToMakeChange.java
+++ StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/numberOfWaysToMakeChange.java
@@ -4,6 +4,9 @@ public class numberOfWaysToMakeChange {
     /*
     * Uses the tabulation methodology
     *
+    * you didn't need to think about max or min here (like minNumberOfCoinsForChange) since you were
+    * purely just looking at the number of ways 
+    *
     * Time: O(n * d) where n is the target size, and d is the number of denominations
     * Space: O(n) cause we have to make tabulation array the size of n at the very beginning
     * */
