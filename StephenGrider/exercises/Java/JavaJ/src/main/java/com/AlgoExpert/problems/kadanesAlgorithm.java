76ebbccd67daa93e105833f2c7e3d41312184854
RedGhoul <avaneesab5@gmail.com>
2020-02-13 08:25:40 -0500

    adding thoughts

--- StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/kadanesAlgorithm.java
+++ StephenGrider/exercises/Java/JavaJ/src/main/java/com/AlgoExpert/problems/kadanesAlgorithm.java
@@ -1,6 +1,17 @@
 package com.AlgoExpert.problems;
 
 public class kadanesAlgorithm {
+    /*
+    * Kind of like common sense since the only thing that could limit your max sum
+    * is a big negative number. So once your sum dips in negative then you know
+    * that adding any more things won't be worth it. So you reset your current sum
+    * and you continue to add. However to mitigate the risk of having a big number at the
+    * end. You also want to continuously keep track of the current max sum every iteration.
+    *
+    * TIME: O(n)
+    * SPACE: O(1)
+    *
+    * */
     public static int kadanesAlgorithmV1(int[] array) {
         if(array.length==0){
             return 0;
