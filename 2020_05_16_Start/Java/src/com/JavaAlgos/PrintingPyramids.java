8e50f15b66b03e609fa87c31fb1496d6b3029520
RedGhoul <avaneesab5@gmail.com>
2020-05-25 09:10:07 -0400

    print problems

---
+++ 2020_05_16_Start/Java/src/com/JavaAlgos/PrintingPyramids.java
@@ -0,0 +1,41 @@
+package com.JavaAlgos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintingPyramids {
+    public static void main(String[] args){
+        printPyramids(23);
+    }
+
+    /**
+     * Q: Accepts a positive number N. Then prints a pyramid shape
+     * with N levels using the "#" character. Make sure the pyramid
+     * has spaces on the left and right.
+     * Input: Integer level
+     * Output: Pyramid printed to the screen
+     * Ideas: Could use arrays lists - Arrays
+     */
+    public static void printPyramids(int level){
+        if(level == 0) System.out.println("");
+        if(level == 1) System.out.println("#");
+        char[] answer = new char[findMaxLevelSize(level)];
+        int mid = ((answer.length-1)/2);
+        answer[mid] = '#';
+        System.out.println(answer);
+        int right = mid+1;
+        int left = mid-1;
+        for(int i = 1; i < level; i++){
+            answer[right] = '#';
+            answer[left] = '#';
+            System.out.println(answer);
+            right++;
+            left--;
+        }
+    }
+
+    public static int findMaxLevelSize(int level){
+        if(level == 0) return 0;
+        return 1 + ((level-1)*2);
+    }
+}
---
+++ 2020_05_16_Start/Java/src/com/JavaAlgos/PrintingSteps.java
@@ -0,0 +1,29 @@
+package com.JavaAlgos;
+
+import java.util.Arrays;
+
+public class PrintingSteps {
+    public static void main(String[] args){
+        printSteps(4);
+    }
+
+    /*
+    * Q: Given a positive number N, print a step shape with N levels
+    * using the "#" character. Each level should have spaces at the
+    * other end.
+    *
+    * Input: numSteps Integer
+    * Output: Steps printed out to the console
+    * Ideas: String Concatenation - ArrayLists - Array
+    * */
+    public static void printSteps(int numSteps){
+        if(numSteps == 0) System.out.println("");
+        char[] outputArr = new char[numSteps]; // when initializing an array,
+        // you put the size thinking it starts from 1
+        Arrays.fill(outputArr,'*');
+        for(int i = 0; i < outputArr.length; i++){
+            outputArr[i] = '#';
+            System.out.println(outputArr);
+        }
+    }
+}
