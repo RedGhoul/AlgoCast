b983587668cf2c7665a15a5609f4bc11aeee74ae
RedGhoul <avaneesab5@gmail.com>
2020-05-30 12:03:29 -0400

    added memoization & iterativeArrayRun

--- 2020_05_16_Start/Java/src/com/JavaAlgos/Fibonacci.java
+++ 2020_05_16_Start/Java/src/com/JavaAlgos/Fibonacci.java
@@ -1,9 +1,31 @@
 package com.JavaAlgos;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Fibonacci {
     public static void main(String[] args){
-        System.out.println(firstTimeRun(12));
-        System.out.println(iteractiveRun(12));
+        int input = 50;
+        long startTime = System.currentTimeMillis();
+        System.out.println(firstTimeRun(input)); // exponential Run Time
+        long endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+
+        startTime = System.currentTimeMillis();
+        System.out.println(iterativeRun(input));
+        endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+
+        startTime = System.currentTimeMillis();
+        System.out.println(iterativeArrayRun(input));
+        endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+
+        startTime = System.currentTimeMillis();
+        System.out.println(memoization(input,null));
+        endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+
     }
 
     public static int firstTimeRun(int n){
@@ -11,7 +33,21 @@ public class Fibonacci {
         return firstTimeRun(n-1) + firstTimeRun(n-2);
     }
 
-    public static int iteractiveRun(int n){
+    public static int memoization(int n, HashMap<Integer,Integer> hash){
+        if(hash == null){
+            hash = new HashMap<Integer, Integer>();
+        }
+        if(n <= 1) return n;
+        if(hash.containsKey(n)){
+            return hash.get(n);
+        }else{
+            Integer answer = memoization(n-1,hash) + memoization(n-2,hash);
+            hash.put(n,answer);
+            return answer;
+        }
+    }
+
+    public static int iterativeRun(int n){
         if(n <= 1) return n;
         int P1 = 0;
         int P2 = 1;
@@ -22,5 +58,19 @@ public class Fibonacci {
             P2 = curAnswer;
         }
         return curAnswer;
+    }
+
+    public static int iterativeArrayRun(int n){
+        if(n<= 1)return n;
+        ArrayList<Integer> fibArray = new ArrayList<Integer>();
+        fibArray.add(0);
+        fibArray.add(1);
+        for(int i = 2; i <= n; i++){
+            Integer p1 = fibArray.get(i-1);
+            Integer p2 = fibArray.get(i-2);
+            Integer newNum = p1+p2;
+            fibArray.add(newNum);
+        }
+        return fibArray.get(n);
     }
 }
