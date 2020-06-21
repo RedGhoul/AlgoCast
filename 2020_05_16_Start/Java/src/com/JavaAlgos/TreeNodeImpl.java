7c48e64ab7d0ea380b91af70391d066887965fc0
RedGhoul <avaneesab5@gmail.com>
2020-06-21 08:47:59 -0400

    did Contains

--- 2020_05_16_Start/Java/src/com/JavaAlgos/TreeNodeImpl.java
+++ 2020_05_16_Start/Java/src/com/JavaAlgos/TreeNodeImpl.java
@@ -6,15 +6,18 @@ public class TreeNodeImpl {
         TreeNodeImpl root_left = new TreeNodeImpl(0);
         TreeNodeImpl root_right = new TreeNodeImpl(12);
         TreeNodeImpl left1 = new TreeNodeImpl(-1);
-        TreeNodeImpl right1 = new TreeNodeImpl(4);
+        TreeNodeImpl right1 = new TreeNodeImpl(20);
 
         root.left = root_left;
         root.right = root_right;
 
         root_left.left = left1;
         root_right.right = right1;
+        root_right.left = new TreeNodeImpl(11);
 
-        root.insert(-20);
+        root.insert(100);
+
+        TreeNodeImpl things = root.Contains(12);
 
     }
     public int data;
@@ -41,6 +44,30 @@ public class TreeNodeImpl {
             }
         }
         return;
+    }
+
+    /**
+     * Assuming it was a valid binary search tree
+     */
+    public TreeNodeImpl Contains(int InputData){
+        if(this.data > InputData){
+            if(this.left == null){
+                return null; // could not find
+            }
+            else{
+                return this.left.Contains(InputData); // got to return everything that returns something
+                // if it doesn't it will default to null, since that is the default value
+            }
+        }else if(this.data < InputData){
+            if(this.right == null){
+                return null;
+            }else{
+                return this.right.Contains(InputData);
+            }
+        }else if(this.data == InputData){
+            return this;
+        }
+        return null;
     }
 
 
