facd87d08d75e82459e7ceafe8a110d31611b1eb
RedGhoul <avaneesab5@gmail.com>
2020-07-04 08:20:28 -0400

    Added some com,ments to basic sorts

--- 2020_05_16_Start/Java/src/com/JavaAlgos/BasicSortingAlgorithms.java
+++ 2020_05_16_Start/Java/src/com/JavaAlgos/BasicSortingAlgorithms.java
@@ -8,7 +8,16 @@ public class BasicSortingAlgorithms {
         System.out.println(Arrays.toString(selectionSort(new int[]{10,0,97,-30,5})));
     }
 
-    
+    /**
+     * The prove me wrong algorithm - O(n^2)
+     * 1) You assume that the current index you are at, is the location of the min value
+     * 2) Then you go through the rest of the array looking for something smaller, and
+     * you keep track of it
+     * 3) And at after going through the rest of the array, if you did find something
+     * that was smaller, you swap it
+     *
+     * And you keep doing that for the rest of the array
+     * **/
     public static int[] selectionSort(int[] inputArray){
         for(int outside = 0; outside < inputArray.length; outside++){
             int IndexOfMin = outside;
@@ -24,6 +33,12 @@ public class BasicSortingAlgorithms {
 
         return inputArray;
     }
+
+    /**
+     * Things always bubble to the top. - O(n^2)
+     * Keep comparing the current value to the others in the list.
+     * If it is greater then swap
+     * **/
     public static int[] bubbleSort(int[] inputArray){
         for(int right = 0; right < inputArray.length;right++){
             for(int left = right+1; left < inputArray.length;left++){
